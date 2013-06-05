a569a354d05b733bc6d3a5e7024846adb0acb132
Miles <miles@bajabound.com>
2013-06-05 16:04:24 -0600

    Update mutil.py

--- mutil.py
+++ mutil.py
@@ -2,13 +2,7 @@
 
 mutil.py
 
-Stands for Miles' Utilities...
-
-What does it sound like it does genius? 
-It's a library of functions I'd like to have
-
-I'm listening to Radiohead, Amnesiac right now.
-I'm a reasonable man, get off my case, get off my case, get off my case
+Stands for Miles' Utilities
 
 """
 
